c3332de183f5fc9eb20d1f1c102de4a8c9a185c1
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2024-04-28 22:25:39 +0200

    [FEATURE] Access control resources for services created.

--- src/infrastructure/db_service.rs
+++ src/infrastructure/db_service.rs
@@ -1,3 +1,11 @@
+use argon2::{
+    password_hash::{
+        rand_core::OsRng,
+        PasswordHash, PasswordHasher, PasswordVerifier, SaltString
+    },
+    Argon2
+};
+
 use std::time::{SystemTime, UNIX_EPOCH};
 
 use crate::{commons::exception::connect_exception::ConnectException, domain::connection_data::ConnectionData, infrastructure::repository::{db_dictionary, i_db_repository::IDBRepository}, service::service::Service};
@@ -8,21 +16,53 @@ use super::repository::e_db_repository::EDBRepository;
 pub struct DBService {
     name: String,
     owner: String,
+    salt: String,
     timestamp: u128,
     connection_data: ConnectionData
 }
 
 impl DBService {
 
-    pub fn new(name: String, owner: String, connection_data: ConnectionData) -> DBService {
+    pub fn new(name: String, owner: String, password: String, connection_data: ConnectionData) -> Result<DBService, ConnectException> {
+        let salt = DBService::generate_salt(password)?;
         let timestamp = SystemTime::now()
             .duration_since(UNIX_EPOCH)
             .expect("Cannot read actual date.")
             .as_millis();
         
-        DBService {
-            name, owner, timestamp, connection_data
+        Ok(DBService {
+            name, salt, owner, timestamp, connection_data
+        })
+    }
+
+    fn generate_salt(password: String) -> Result<String, ConnectException> {
+        let salt = SaltString::generate(&mut OsRng);
+
+        let argon2 = Argon2::default();
+
+        let result = argon2.hash_password(password.as_bytes(), &salt);
+        if result.is_err() {
+            let exception = ConnectException::new(result.unwrap_err().to_string());
+            return Err(exception);
         }
+
+        Ok(result.unwrap().to_string())
+    }
+
+    pub fn is_authorized(&self, password: String) -> Result<(), ConnectException> {
+        let parsed_hash = PasswordHash::new(&self.salt);
+        if parsed_hash.is_err() {
+            let exception = ConnectException::new(parsed_hash.unwrap_err().to_string());
+            return Err(exception);
+        }
+        
+        let result = Argon2::default().verify_password(password.as_bytes(), &parsed_hash.unwrap());
+        if result.is_err() {
+            let exception = ConnectException::new(result.unwrap_err().to_string());
+            return Err(exception);
+        }
+
+        Ok(())
     }
     
     pub fn name(&self) -> String {
@@ -31,6 +71,10 @@ impl DBService {
 
     pub fn category(&self) -> EDBRepository {
         self.connection_data.category()
+    }
+
+    pub fn salt(&self) -> String {
+        self.salt.clone()
     }
 
     pub async fn instance(&self) -> Result<Service<impl IDBRepository>, ConnectException> {
